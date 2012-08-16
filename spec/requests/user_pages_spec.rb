89b87f035fbafa58780ad6414d43f8d33e69a671
Joao Mello <joao.mello@dtmtec.com.br>
2012-08-16 15:21:20 -0300

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,7 +19,7 @@ describe "User pages" do
     it { should have_selector('title',  text: user.name) }
   end
 
-describe "signup" do
+  describe "signup" do
 
     before { visit signup_path }
 
@@ -29,14 +29,30 @@ describe "signup" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+        fill_in "Name",                with: "Example User"
+        fill_in "Email",                 with: "user@example.com"
+        fill_in "Password",          with: "foobar"
+        fill_in "Confirmation",    with: "foobar"
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
 
       it "should create a user" do
@@ -44,5 +60,4 @@ describe "signup" do
       end
     end
   end
-
 end
